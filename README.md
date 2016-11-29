31c8826b11d3565601a3bb12cd910982134c84e3
eric thul <thul.eric@gmail.com>
2016-11-28 20:46:50 -0500

    Initial sketch of a free applicative implementation
    The goal of this implementation is to improve performance
    characteristics of the operations of the free applicative functor in
    PureScript.
    
    Reference #7
    
    Reference #8

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Free applicative functors for PureScript.
 
 See the following reference for further information.
 * [Free Applicative Functors](http://arxiv.org/abs/1403.0749) (Capriotti and Kaposi 2014)
+* [Free Applicative Functors in Haskell](https://www.eyrie.org/~zednenem/2013/05/27/freeapp) (Menendez 2013)
+* [The fraxl package](https://hackage.haskell.org/package/fraxl) (Fancher 2016)
 
 ## Installation
 
--- src/Control/Applicative/Free.purs
+++ src/Control/Applicative/Free.purs
@@ -1,5 +1,15 @@
+-- | This module defines a free applicative functor.
+-- |
+-- | The implementation of this module is based on Dave Menendez and
+-- | Will Fancher's work.
+-- |
+-- | See [Free Applicative Functors in Haskell](https://www.eyrie.org/~zednenem/2013/05/27/freeapp) (Menendez 2013)
+-- |
+-- | See [The fraxl package](https://hackage.haskell.org/package/fraxl) (Fancher 2016)
 module Control.Applicative.Free
   ( FreeAp
+  , freeAp
+  , unFreeAp
   , liftFreeAp
   , retractFreeAp
   , foldFreeAp
@@ -9,53 +19,51 @@ module Control.Applicative.Free
 
 import Prelude hiding (ap)
 
+import Data.ApList (ApList)
+import Data.ApList as ApList
 import Data.Const (Const(..))
-import Data.Exists (Exists, mkExists, runExists)
 import Data.Monoid (class Monoid)
 import Data.Newtype (unwrap)
+import Data.Tuple (Tuple(..))
 
 -- | The free applicative functor for a type constructor `f`.
-data FreeAp f a = Pure a | Ap (Exists (ApF f a))
+newtype FreeAp f a = FreeAp (forall u y z. (forall x. (x -> y) -> ApList f x -> z) -> (u -> a -> y) -> ApList f u -> z)
 
-data ApF f a i = ApF (Unit -> f i) (Unit -> FreeAp f (i -> a))
+freeAp :: forall f a. (forall u y z. (forall x. (x -> y) -> ApList f x -> z) -> (u -> a -> y) -> ApList f u -> z) -> FreeAp f a
+freeAp = FreeAp
 
-ap :: forall f a i. (Unit -> f i) -> (Unit -> FreeAp f (i -> a)) -> FreeAp f a
-ap v k = Ap (mkExists (ApF v k))
+unFreeAp :: forall f a. FreeAp f a -> (forall u y z. (forall x. (x -> y) -> ApList f x -> z) -> (u -> a -> y) -> ApList f u -> z)
+unFreeAp (FreeAp x) = x
 
 -- | Lift a value described by the type constructor `f` into
 -- | the free applicative functor.
 liftFreeAp :: forall f a. f a -> FreeAp f a
-liftFreeAp a = ap (\_ -> a) (\_ -> Pure id)
+liftFreeAp a = freeAp (\k f s -> k (\(Tuple a' s') -> f s' a') (ApList.cons a s))
 
 -- | Run a free applicative functor using the applicative instance for
 -- | the type constructor `f`.
 retractFreeAp :: forall f a. Applicative f => FreeAp f a -> f a
-retractFreeAp (Pure a) = pure a
-retractFreeAp (Ap x) = runExists (\(ApF v k') -> apply (retractFreeAp (k' unit)) (v unit)) x
+retractFreeAp fa = unFreeAp fa (\f s -> f <$> ApList.reduce s) (\_ -> id) ApList.nil
 
 -- | Run a free applicative functor with a natural transformation from
 -- | the type constructor `f` to the applicative functor `g`.
 foldFreeAp :: forall f g a. Applicative g => (f ~> g) -> FreeAp f a -> g a
-foldFreeAp k (Pure a) = pure a
-foldFreeAp k (Ap x) = runExists (\(ApF v k') -> apply (map (flip id) (k (v unit))) (foldFreeAp k (k' unit))) x
+foldFreeAp k = retractFreeAp <<< hoistFreeAp k
 
 -- | Natural transformation from `FreeAp f a` to `FreeAp g a` given a
 -- | natural transformation from `f` to `g`.
 hoistFreeAp :: forall f g a. (f ~> g) -> FreeAp f a -> FreeAp g a
-hoistFreeAp k (Pure a) = Pure a
-hoistFreeAp k (Ap x) = runExists (\(ApF v k') -> ap (\_ -> k (v unit)) (\_ -> hoistFreeAp k (k' unit))) x
+hoistFreeAp g x = freeAp (\k f s -> unFreeAp x (\f' s' -> ApList.rebase (ApList.hoist g s') k (\v u -> f v (f' u)) s) (const id) ApList.nil)
 
 -- | Perform monoidal analysis over the free applicative functor `f`.
 analyzeFreeAp :: forall f m a. Monoid m => (forall b. f b -> m) -> FreeAp f a -> m
 analyzeFreeAp k = unwrap <<< foldFreeAp (Const <<< k)
 
 instance functorFreeAp :: Functor (FreeAp f) where
-  map k (Pure a) = Pure (k a)
-  map k (Ap x) = runExists (\(ApF v k') -> ap v (\_ -> map ((<<<) k) (k' unit))) x
+  map g x = freeAp (\k f -> unFreeAp x k (\s -> f s <<< g))
 
 instance applyFreeAp :: Apply (FreeAp f) where
-  apply (Pure k) f = map k f
-  apply (Ap x) f = runExists (\(ApF v k') -> ap v (\_ -> apply (map flip (k' unit)) f)) x
+  apply x y = freeAp (\k f -> unFreeAp y (unFreeAp x k) (\s a g -> f s (g a)))
 
 instance applicativeFreeAp :: Applicative (FreeAp f) where
-  pure = Pure
+  pure a = freeAp (\k f -> k (_ `f` a))
