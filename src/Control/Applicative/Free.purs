7e7c89da98c803290d595309eedd7d10cdcf8a3b
Irakli Safareli <i.safareli@gmail.com>
2017-12-22 18:17:54 +0100

    tmp work

--- src/Control/Applicative/Free.purs
+++ src/Control/Applicative/Free.purs
@@ -7,55 +7,139 @@ module Control.Applicative.Free
   , analyzeFreeAp
   ) where
 
-import Prelude hiding (ap)
-
 import Data.Const (Const(..))
-import Data.Exists (Exists, mkExists, runExists)
+import Data.Either (Either(..))
+import Data.List (List(..))
+import Data.List.NonEmpty as NEL
 import Data.Monoid (class Monoid)
 import Data.Newtype (unwrap)
+import Data.NonEmpty ((:|))
+import Data.Tuple (Tuple(..))
+import Debug.Trace (spy)
+import Prelude hiding (ap)
+import Unsafe.Coerce (unsafeCoerce)
 
 -- | The free applicative functor for a type constructor `f`.
-data FreeAp f a = Pure a | Ap (Exists (ApF f a))
+data FreeAp f a
+  = Pure a
+  | Lift (f a)
+  | Ap (FreeAp f (Val -> a)) (FreeAp f Val)
 
-data ApF f a i = ApF (Unit -> f i) (Unit -> FreeAp f (i -> a))
-
-ap :: forall f a i. (Unit -> f i) -> (Unit -> FreeAp f (i -> a)) -> FreeAp f a
-ap v k = Ap (mkExists (ApF v k))
+data Val
 
 -- | Lift a value described by the type constructor `f` into
 -- | the free applicative functor.
 liftFreeAp :: forall f a. f a -> FreeAp f a
-liftFreeAp a = ap (\_ -> a) (\_ -> Pure id)
+liftFreeAp = Lift
 
--- | Run a free applicative functor using the applicative instance for
--- | the type constructor `f`.
-retractFreeAp :: forall f a. Applicative f => FreeAp f a -> f a
-retractFreeAp (Pure a) = pure a
-retractFreeAp (Ap x) = runExists (\(ApF v k') -> apply (retractFreeAp (k' unit)) (v unit)) x
+type ApFunc g = { func :: g (Val -> Val), count :: Int }
+type FuncStack g = List (ApFunc g)
+type ValStack f = NEL.NonEmptyList (FreeAp f Val)
+type Stack f g = Tuple (FuncStack g) (ValStack f)
 
 -- | Run a free applicative functor with a natural transformation from
 -- | the type constructor `f` to the applicative functor `g`.
 foldFreeAp :: forall f g a. Applicative g => (f ~> g) -> FreeAp f a -> g a
-foldFreeAp k (Pure a) = pure a
-foldFreeAp k (Ap x) = runExists (\(ApF v k') -> apply (map (flip id) (k (v unit))) (foldFreeAp k (k' unit))) x
+foldFreeAp nat z =
+  unsafeToG $ go $ Tuple Nil (NEL.singleton $ unsafeToFVal z)
+  where
+  unsafeToG :: g Val -> g a
+  unsafeToG = unsafeCoerce
+
+  unsafeToFVal :: forall f' a'. FreeAp f' a' -> FreeAp f' Val
+  unsafeToFVal = unsafeCoerce
+
+  go :: Stack f g -> g Val
+  go stck@(Tuple fStack (NEL.NonEmptyList (val :| vals))) =
+    let zzz = stck
+    in case val of
+      Pure a -> case goApply fStack vals (pure a) of
+        Left x -> x
+        Right s -> go s
+      Lift a -> case goApply fStack vals (nat a) of
+        Left x -> x
+        Right s -> go s
+      Ap l r ->
+        let nextVals = NEL.NonEmptyList (r :| vals)
+        in go $ goLeft fStack nextVals nat l 1
+
+goApply
+  :: forall f g
+  . Applicative g
+  => FuncStack g
+  -> List (FreeAp f Val)
+  -> g Val
+  -> Either (g Val) (Stack f g)
+goApply fStack vals gVal =
+  case fStack of
+    Nil -> Left gVal
+    Cons f fs ->
+      let gRes = f.func <*> gVal
+      in case vals of
+        Nil -> Left gRes
+        Cons val vals' ->
+          if f.count == 1 then
+            case fs of
+              Nil -> Left gRes
+              _ -> goApply fs vals gRes
+          else Right $ Tuple
+            (Cons { func: unsafeToGFunc gRes, count: f.count - 1 } fs)
+            (NEL.NonEmptyList (val :| vals'))
+  where
+  unsafeToGFunc :: g Val -> g (Val -> Val)
+  unsafeToGFunc = unsafeCoerce
+
+goLeft
+  :: forall f g
+  . Applicative g
+  => FuncStack g
+  -> ValStack f
+  -> (f ~> g)
+  -> FreeAp f (Val -> Val)
+  -> Int
+  -> Stack f g
+goLeft fStack valStack nat func count = case func of
+  Pure a -> Tuple (Cons { func: pure a, count } fStack) valStack
+  Lift a -> Tuple (Cons { func: nat a, count } fStack) valStack
+  Ap l r -> goLeft fStack (NEL.cons r valStack) nat (unsafeToFunc l) (count + 1)
+  where
+  unsafeToFunc :: FreeAp f (Val -> Val -> Val) -> FreeAp f (Val -> Val)
+  unsafeToFunc = unsafeCoerce
+
+-- | Run a free applicative functor using the applicative instance for
+-- | the type constructor `f`.
+retractFreeAp :: forall f a. Applicative f => FreeAp f a -> f a
+retractFreeAp = foldFreeAp id
 
 -- | Natural transformation from `FreeAp f a` to `FreeAp g a` given a
 -- | natural transformation from `f` to `g`.
 hoistFreeAp :: forall f g a. (f ~> g) -> FreeAp f a -> FreeAp g a
-hoistFreeAp k (Pure a) = Pure a
-hoistFreeAp k (Ap x) = runExists (\(ApF v k') -> ap (\_ -> k (v unit)) (\_ -> hoistFreeAp k (k' unit))) x
+hoistFreeAp f = foldFreeAp (f >>> liftFreeAp)
 
 -- | Perform monoidal analysis over the free applicative functor `f`.
 analyzeFreeAp :: forall f m a. Monoid m => (forall b. f b -> m) -> FreeAp f a -> m
 analyzeFreeAp k = unwrap <<< foldFreeAp (Const <<< k)
 
+mkAp :: forall f a b. FreeAp f (b -> a) -> FreeAp f b -> FreeAp f a
+mkAp fba fb = Ap (coerceFunc fba) (coerceValue fb)
+  where
+  coerceFunc :: FreeAp f (b -> a) -> FreeAp f (Val -> a)
+  coerceFunc = unsafeCoerce
+
+  coerceValue :: FreeAp f b -> FreeAp f Val
+  coerceValue = unsafeCoerce
+
 instance functorFreeAp :: Functor (FreeAp f) where
-  map k (Pure a) = Pure (k a)
-  map k (Ap x) = runExists (\(ApF v k') -> ap v (\_ -> map ((<<<) k) (k' unit))) x
+  map f x = mkAp (Pure f) x
 
 instance applyFreeAp :: Apply (FreeAp f) where
-  apply (Pure k) f = map k f
-  apply (Ap x) f = runExists (\(ApF v k') -> ap v (\_ -> apply (map flip (k' unit)) f)) x
+  apply fba fb = mkAp fba fb
 
 instance applicativeFreeAp :: Applicative (FreeAp f) where
   pure = Pure
+
+instance showFreeAp :: Show (FreeAp f a) where
+  show = case _ of
+    Pure a -> "(Pure Val)"
+    Lift f -> "(Lift F)"
+    Ap l r -> "(Ap " <> show l <> " " <> show r <> ")"
--- test/Test/Control/Applicative/Free.purs
+++ test/Test/Control/Applicative/Free.purs
@@ -2,11 +2,29 @@ module Test.Control.Applicative.Free
   ( checkAnalyze
   ) where
 
-import Prelude (Unit, (==), (<>), unit, apply, map)
-import Control.Applicative.Free (FreeAp, liftFreeAp, analyzeFreeAp)
+-- import Prelude (Unit, (==), (<>), unit, (-), ($), (>>>))
+import Prelude
+
+import Control.Applicative ((*>))
+import Control.Applicative.Free (FreeAp, liftFreeAp, analyzeFreeAp, retractFreeAp)
 import Data.Either (Either(..))
+import Data.Identity (Identity(..))
+import Data.Show (class Show)
+import Debug.Trace (spy)
 
 data M r = A r | B r
+
+instance showFreeAp :: Show (M a) where
+  show (A _) = "A"
+  show (B _) = "B"
+
+fInc = liftFreeAp (Identity (_ + 1))
+fOne = liftFreeAp (Identity 1)
+fMult = liftFreeAp (Identity (+))
+
+x = fMult <*> (fInc <*> fOne) <*> (fInc <*> fOne)
+
+zzz = spy { res1: retractFreeAp x }
 
 ma :: FreeAp M Unit
 ma = liftFreeAp (A unit)
@@ -15,15 +33,27 @@ mb :: FreeAp M Unit
 mb = liftFreeAp (B unit)
 
 printM :: forall a. FreeAp M a -> String
-printM = analyzeFreeAp go where
+printM fr =
+  analyzeFreeAp (go >>> spy) fr
+  where
   go (A _) = "A"
   go (B _) = "B"
 
+build :: Int -> FreeAp M Unit -> FreeAp M Unit -> FreeAp M Unit
+build 0 _ acc = acc
+build n x acc = build (n - 1) x (acc *> x)
+
 result :: String
-result = printM (apply (map (\l r -> r) ma) mb)
+result = printM (build 10 (ma *> mb) mb)
+-- result = printM ((mb *> mb) *> ma)
+-- result = printM (mb *> (mb *> ma))
+
+buildExpected :: Int -> String -> String -> String
+buildExpected 0 _ acc = acc
+buildExpected n x acc = buildExpected (n - 1) x (acc <> x)
 
 expected :: String
-expected = "AB"
+expected = buildExpected 10 "AB" "B"
 
 checkAnalyze :: Either String String
 checkAnalyze = if result == expected
